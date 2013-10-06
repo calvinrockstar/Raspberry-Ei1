3cc6b5d5ab2ebce4e69a91088415b6636069030e
Calvinrockstar <calvinrockstar@gmail.com>
2013-10-06 16:41:40 +0200

    Working loop

--- Eileen1_POC.py
+++ Eileen1_POC.py
@@ -9,12 +9,14 @@ from subprocess import PIPE, STDOUT
 
 ser = Serial('/dev/ttyACM0', 9600, timeout=1)
 isPlaying = 0
-Movies = [['vid1.mov',15],['vid2.mov',15],['vid1.mov',15]]
- 
+#Movies = [['/home/pi/Desktop/Sensor/vid1.mov',15],['/home/pi/Desktop/Sensor/vid2.mov',15],['/home/pi/Desktop/Sensor/vid1.mov',15]]
+Movies = [['/home/pi/Desktop/Sensor/vid1.mov',15]]
+
 def PlayList(Movies):
 	for movie in Movies:
 		p = subprocess.Popen(["omxplayer", movie[0]])
 		time.sleep(movie[1])
+		p.kill()
 		#PlayList(Movies)
 
 def receiving(ser):
@@ -38,20 +40,35 @@ def receiving(ser):
 	    	  
 	    if last_received > '50' and last_received < '80':
 			startVideo = startVideo + 1
-			print(startVideo)
+			#print(startVideo)
 		
-            		if startVideo > 3:	
+            		if startVideo > 0:	
 	    				isPlaying = 1
 	    				PlayList(Movies)
+	    				
+	    				#subprocess.call(['omxplayer','/home/pi/Desktop/Sensor/vid1.mov'])
+	    				#time.sleep(16)
+	    				#child.sendline('q')
+	    				#subprocess.call(['omxplayer','--pos'])
+	    				
+	    				#command = 'omxplayer -r -o local /home/pi/Desktop/Sensor/vid1.mov'
+	    				#child = pexpect.spawn(command)
+	    				#time.sleep(10)
+	    				#child.sendline('p')
+
+	    				#subprocess.Popen(["omxplayer",'vid1.mov'])
+	    				#PlayList(Movies)
 	    				#subprocess.Popen("/home/pi/Desktop/Sensor/vidtest.py 1",shell=True)
 					#command = 'omxplayer -r -o local /home/pi/Desktop/Sensor/vid1.mov'
 					#child = pexpect.spawn(command)
-
+					#time.sleep(2)					
+					#child.sendline('p')	
+					#subprocess.call(['omxplayer','bg black'])
 					#subprocess.call(['omxplayer','/home/pi/Desktop/Sensor/vid1.mov'])
+						
 					#pexpect.run('p')
 					#child = pexpect.spawn (['omxplayer','/home/pi/Desktop/Sensor/vid1.mov'])
 					#child.expect('p')
-					#child.sendline('p')	
 					
 					#subprocess.call("bash /home/pi/Desktop/Sensor/videoplayer1.sh", shell=True)
 					#time.sleep(16)	
