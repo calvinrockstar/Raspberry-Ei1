2b45ded44c5857181f3fd9eee79a9203d5c8ae3d
Calvinrockstar <calvinrockstar@gmail.com>
2013-10-24 20:49:04 +0200

    Capsense working

---
+++ Touch_Sensor_Dark_working_24_10_2013.ino
@@ -0,0 +1,33 @@
+#include <CapSense.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10M between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ */
+
+
+CapSense   cs_4_2 = CapSense(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+void setup()                    
+{
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  //   pinMode(led, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop()                    
+{
+  long start = millis();
+  long total1 =  cs_4_2.capSense(30);
+
+  unsigned long end = millis(); // Using the proper type
+
+  Serial.println(total1);                  // print sensor output 1
+  delay(50);
+  //Serial.print("\t");
+
+}
+
